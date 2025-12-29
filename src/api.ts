b2dfb43e7ece2d0d953e44fa8a3cb75f3920931b
isaacmdrs <isaac.gomes.medeiros@gmail.com>
2025-12-28 21:36:00 -0300

    consome url

--- src/api.ts
+++ src/api.ts
@@ -1,10 +1,15 @@
 import axios from 'axios';
 
+// Define a URL dinamicamente
+// Se existir a variável de ambiente (Vercel), usa ela. 
+// Senão, usa o localhost (Seu PC).
+const apiUrl = import.meta.env.VITE_API_URL || 'http://localhost:8080/api';
+
 export const api = axios.create({
-  baseURL: 'http://localhost:8080/api', // URL do seu Spring Boot
+  baseURL: apiUrl,
 });
 
-// Interceptor para adicionar o Token automaticamente em toda requisição
+// Interceptor (Mantenha igual, está ótimo)
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('serra_token');
   if (token) {
